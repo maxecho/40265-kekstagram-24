f753e4f26c282616ead4fcd21dfa3695c54504a6
Yaroslav Lashch <yaroslav.lashch@gmail.com>
2021-10-29 21:25:14 +0300

    useful js added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-/* Hope i'll make it just on time */
+/* Выводит рандомное целое число из диапазона */
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+random(0,1000);
+
+/* Функция для проверки максимальной длины строки. */
+const maxComment = function(Comment,maxCommentSize){
+  if (Comment<=maxCommentSize){return 'true';}
+  else {return 'false';}
+};
+
+maxComment(5,10);
